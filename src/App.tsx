53da28f8603c1f92f28a4d16158f09a3f0758a98
Šimon Janča <jancikuvmail@gmail.com>
2025-11-15 09:41:51 +0100

    Config emotion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react';
-import './App.css'
+import { useQuery } from '@tanstack/react-query';
+import styled from '@emotion/styled';
+import { ClipLoader } from "react-spinners";
+
 import type { User } from './components/employeeCard/employeeCard.comp';
 import { EmployeeList } from './components/employeeList/employeeList.comp'
-import { useQuery } from '@tanstack/react-query';
 import fetchUsers from './utils/users.api';
 import ConcatUserFilterCallback from './utils/users.utils';
+
+import './App.css'
 
 function App() {
   const [employees, setEmployees] = useState<User[]>([]);
@@ -24,12 +28,22 @@ function App() {
     }
   }, [data, filterString])
 
+  const CenteredContainer = styled.div`
+    display: flex;
+    background-color: ${isLoading ? '#f0f0f0' : 'transparent'};
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  `;
+
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <CenteredContainer>
+      <ClipLoader size={42} />
+    </CenteredContainer>;
   }
 
   if (isError) {
-    return <div>Error: {error instanceof Error ? error.message : 'Unknown error'}</div>;
+    return <CenteredContainer>Error: {error instanceof Error ? error.message : 'Unknown error'}</CenteredContainer>;
   }
 
   return <EmployeeList employees={employees} setFilter={setFilterString} />
